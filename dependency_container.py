5f2faa1197eeaf076c0cf76dfb6ff0763dc3eb19
Optimus80 <161043500+Optimus80@users.noreply.github.com>
2026-07-17 10:33:10 -0300

    Create dependency_container.py

---
+++ dependency_container.py
@@ -0,0 +1,13 @@
+class Container:
+
+    def __init__(self):
+
+        self.services = {}
+
+    def register(self, name, service):
+
+        self.services[name] = service
+
+    def resolve(self, name):
+
+        return self.services[name]
