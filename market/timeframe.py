7dd8c306f601ebaab8c1c88b33a5a378daf07041
Optimus80 <161043500+Optimus80@users.noreply.github.com>
2026-07-17 12:56:13 -0300

    Add TimeFrame enum for trading intervals

---
+++ market/timeframe.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class TimeFrame(Enum):
+
+    M1 = "1m"
+
+    M3 = "3m"
+
+    M5 = "5m"
+
+    M15 = "15m"
+
+    M30 = "30m"
+
+    H1 = "1h"
+
+    H4 = "4h"
+
+    D1 = "1d"
